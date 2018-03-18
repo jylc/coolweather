c633aa5c57c9fe6949b12fc39830c4912a805c65
jylc <jylc@gmail.com>
2018-03-18 09:46:33 +0800

    添加界面

--- app/src/main/java/util/Utility.java
+++ app/src/main/java/util/Utility.java
@@ -2,6 +2,8 @@ package util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -9,6 +11,7 @@ import org.json.JSONObject;
 import db.City;
 import db.County;
 import db.Province;
+import gson.Weather;
 
 /**
  * Created by wxb on 2018/3/16.
@@ -74,5 +77,22 @@ public class Utility {
             }
         }
         return false;
+    }
+
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     * */
+
+    public static Weather handleWeatherResponse(String response){
+        try{
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
